ead2dbcfa76aec97697887942d8af44bd7d36c93
GeorgeH <georgeharland12@gmail.com>
2023-07-28 13:53:49 +0100

    fixes

--- src/index.ts
+++ src/index.ts
@@ -21,20 +21,18 @@ const handleUserInput = async () => {
   const numRovers = parseInt(await askQuestion('Enter the number of rovers (eg. 2): '));
   const outputs: string[] = []
   for (let i = 0; i < numRovers; i++) {
-    const roverPosition = await askQuestion('Enter start position and direction string for rover ' + i+1 + ' (e.g., "1 2 N"): ');
+    const roverPosition = await askQuestion('Enter start position and direction string for rover ' + (i+1) + ' (e.g., "1 2 N"): ');
     const [roverX, roverY, roverDir] = roverPosition.split(' ');
     const numX = parseInt(roverX);
     const numY = parseInt(roverY);
     const rover = new Rover(numX, numY, roverDir as CardinalDirectionsType, environment);
-    console.log(rover.x, rover.y, rover.direction);
     const roverCommands = await askQuestion('Enter the command string for the rover (eg. LMRM): ');
     const finalPosition = rover.runFullInput(roverCommands);
-    console.log(finalPosition)
     outputs.push(finalPosition.x.toString() + ' ' + finalPosition.y.toString() + ' ' + finalPosition.direction);
   }
 
+  console.log('Final positions of Rovers:');
   outputs.forEach((position, index) => {
-    console.log('Final position for Rover ' + index+1 + ': ');
     console.log(position);
   });
 }
